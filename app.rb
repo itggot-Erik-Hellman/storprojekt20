bcfe72c1e9ecdd36e8942f667c53ce62bd3f8c38
itggot-Erik-Hellman <59963407+itggot-Erik-Hellman@users.noreply.github.com>
2020-05-07 13:45:15 +0200

    update

--- app.rb
+++ app.rb
@@ -2,25 +2,25 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
-
+#Hämtar in sinatra, slim, SQL, och bcrypt
 enable :sessions
-
+#Enablar sessions med sinatra
 db = SQLite3::Database.new("db/workflow.db")
 db.results_as_hash = true
 
-get ('/') do
+get ('/') do #Gör så att informationen som visas i routen '/' är från slim filen "start.slim"
     slim(:start)
 end
 
-post ('/users/create') do
+post ('/users/start/create') do #Routen '/users/create' lägger till ett email samt ett lösenord som crypteras av bcrypt. Http-metoden är post eftersom man ändrar på något, i detta fall databasen där man lägger till email och password
     email = params[:email]
     password = params[:password]
-    password_digest = BCrypt::Password.create(password)
-    db.execute("INSERT INTO users (email, password_digest) VALUES (?,?)",email,password_digest)
-    redirect('/')
+    password_digest = BCrypt::Password.create(password) #crypterar det lösenord användaren skrev in i hemsidan
+    db.execute("INSERT INTO users (email, password_digest) VALUES (?,?)",email,password_digest) #lägger in det email och lösenord användaren har skrivit in till table: "users" attribut: "email" och "password_digest"
+    redirect('/') #dirigerar om användaren tillbaka till startsidan efter att den har gjort sitt konto
 end
 
-post ('/users/login') do
+post ('/users/start/login') do #Routen '/users/login' Kollar om det användaren har skrivit in i email och lösenord stämmer översäns med det som finns i databasen "authentication". Om det gör det kommer användaren in till hemsidan: '/main_page' om inte så redigeras han tillbaka till startsidan
     result = db.execute("SELECT id, password_digest FROM users WHERE email=?", params[:email])
 
     if(BCrypt::Password.new(result.first["password_digest"]) == params[:password])
@@ -31,35 +31,35 @@ post ('/users/login') do
     end
 end
 
-get ('/main_page') do
+get ('/main_page') do #routen '/main_page' är min första get http-metod. Detta då '/main_page' bara ska visa information.
     db.results_as_hash = true
     destination = db.execute("SELECT * FROM category")
-    slim(:main_page, locals:{destination:destination})
+    slim(:main_page, locals:{destination:destination}) #Gör så att informationen inuti '/main_page' är från slim filen "main_page.slim" samt skickar med destination
 end
 
-get ('/logo') do
+get ('/logo') do #visar min logo samt när man trycker på den så redigeras man till '/main_page'
     redirect('/main_page')
 end
 
-post ('/new') do
+post ('/new') do #Skapar en ny category_name vilket är vart man har åkt
     id = params[:id]
     category_name = params[:category_name]
     db = SQLite3::Database.new("db/workflow.db") 
-    create = db.execute("INSERT INTO category (id, category_name) VALUES (?,?)",id,category_name)
+    create = db.execute("INSERT INTO category (id, category_name) VALUES (?,?)",id,category_name) #Lägger in det man har skrivit in i table:category och attributerna "id" och "category_name"
     redirect('/main_page')
 end
 
-post ('/delete/:id') do
+post ('/delete/:id') do #Tar bort en hel rad i tabellen:category. Använder mig även av en dynamisk route för att inte behöve skriva samma kod för varje rad i category
     id = params[:id]
     db = SQLite3::Database.new("db/workflow.db") 
-    db.execute("DELETE FROM category WHERE id=?",id.to_i)
+    db.execute("DELETE FROM category WHERE id=?",id.to_i) #tar bort från tabellen category beroende på vilket id man tillkallar
     redirect('/main_page')
 end
 
-post ('/update/:id') do
+post ('/update/:id') do #updaterar category_name i tabellen category
     id = params[:id]
     category_name = params[:category_name]
     db = SQLite3::Database.new("db/workflow.db")
-    db.execute("UPDATE category SET category_name=? WHERE id=?", category_name,id.to_i)
+    db.execute("UPDATE category SET category_name=? WHERE id=?", category_name,id.to_i) #updaterar informationen i category_name beroende på vad användaren har skrivit (kan ändra Tyskland till tex Finland eller vad som helst)
     redirect('/main_page')
 end
--- projektplan.md
+++ projektplan.md
@@ -1,11 +1,20 @@
 # Projektplan
 
 ## 1. Projektbeskrivning (Beskriv vad sidan ska kunna göra)
-Hemsidan ska ha en inloggningsfuntion samt en regristreringsfuntion. Man ska sedan kunna gå vidare till hemsidan med produkter efter att man har loggat in, där ska man även kunna ändra sitt email samt lösenord. Sedan så har jag en admin som kan ändra och crud'a på produkter.
-
+Hemsidan ska ha en inloggningsfuntion samt en regristreringsfuntion. När man sedan har loggat in ska man komma till "main pagen" där man ska kunna lägga till resor samt redigera dem och ta bort dem.
 ## 2. Vyer (visa bildskisser på dina sidor)
-
+Bildskiss.jpg
+![Bildskiss](Bildskiss.jpg)
 ## 3. Databas med ER-diagram (Bild)
 Flowchart.png
 ![Flowchart](Flowchart.png)
+
+I och med att jag ändrade vad min hemsida skule göra efter att jag gjorde min flowchart samt databas så ledde det till att jag har vissa tables och attribut har namn som inte riktigt passar in. Tex attributet "category_name" som i min hemsida nu är rese destinationen. Jag gjorde inte heller klart alla funktioner som hela hemsidan skulle ha använd i och med att jag inte hade tid vilket ledde till att jag inte använder alla tables och attribut.
 ## 4. Arkitektur (Beskriv filer och mappar - vad gör/inehåller de?)
+db mappen innehåller min db browser databas där jag har information som tex email och lösenord
+
+Public mappen innehåller img mappen vilket har de bilder jag använder för min sida. Bilderna är i public i och med att alla användare ska se dem.
+
+Views mappen innehåller allt mitt "front end" dvs det som ska visas på hemsidan. Inne i mappen finns mina slim filer som innehåller det som visas på min main page och inlåggningsida.
+
+app.rb är hjärnan av allt, det är där man bestämmer vad proggrammet gör och vad som händer när man går in på de andra delsidorna genom att definera routes.
